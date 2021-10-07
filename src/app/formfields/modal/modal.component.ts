2b30719b1804481ddf9e2ea413dd79dd1c546449
Sandor Toth <sandor.toth@hotmail.com>
2021-10-07 22:19:29 +0200

    modal component created

---
+++ src/app/formfields/modal/modal.component.ts
@@ -0,0 +1,21 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
+})
+export class ModalComponent implements OnInit {
+
+  constructor(public dialogRef: MatDialogRef<ModalComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
